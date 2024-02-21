e238342846854539a367f69299320ab9d51d3a55
Rafael pereira de oliveira <101524748+oliveira-dos-anjos@users.noreply.github.com>
2024-02-21 19:58:11 -0300

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+#app.config['SQLAlCHEMY_DATABASE_URI'] = ''
+
+#login_manager = LoginManager(app)
+#db = SQLAlchemy(app)
---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Processar os dados do formulário de login
+        return "Processando dados de login..."
+
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Processar os dados do formulário de registro
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/recuperar', methods=['GET', 'POST'])
+def recuperar():
+    if request.method == 'POST':
+        return redirect(url_for('redefinir'))
+
+    return render_template('recuperar.html')
+
+@app.route('/redefinir', methods=['GET', 'POST'])
+def redefinir():
+    if request.method == 'POST':
+        # Processar os dados do formulário de redefinição de senha
+        return redirect(url_for('login'))  # Redirecionar para a tela de login após redefinição de senha
+
+    return render_template('redefinir.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
