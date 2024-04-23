8d61043d77d3f13360089e537ec4efec6095f7b4
Rafael pereira de oliveira <101524748+oliveira-dos-anjos@users.noreply.github.com>
2024-04-23 06:35:19 -0300

    Atualizar o home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,28 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var tableContainer = document.querySelector('.lojas-container');
-    var windowHeight = window.innerHeight;
-    tableContainer.style.height = windowHeight + 'px';
+            var tableContainer = document.getElementById('lojas-container');
+            var windowHeight = window.innerHeight;
+            tableContainer.style.height = windowHeight + 'px';
 
-    tableContainer.addEventListener('scroll', function() {
-        if (document.fullscreenElement === null) { // Verifica se já está em tela cheia
-            try {
-                // Tenta entrar no modo de tela cheia
-                tableContainer.requestFullscreen();
-            } catch (error) {
-                console.error('Erro ao tentar entrar em tela cheia:', error);
-            }
-        }
-    });
+            tableContainer.addEventListener('scroll', function() {
+                if (document.fullscreenElement === null) {
+                    try {
+                        tableContainer.requestFullscreen();
+                    } catch (error) {
+                        console.error('Erro ao tentar entrar em tela cheia:', error);
+                    }
+                }
+            });
 
-    var profileLink = document.getElementById('profile-link');
-    profileLink.addEventListener('click', function(event) {
-        var isLoggedIn = profileLink.getAttribute('data-logged-in') === 'true';
-        if (!isLoggedIn) {
-            return;
-        }
-        event.preventDefault();
-        var dropdownContent = document.querySelector('.dropdown-content');
-        dropdownContent.style.display = 'block';
-    });
-});
-
+            var profileLink = document.getElementById('profile-link');
+            profileLink.addEventListener('click', function(event) {
+                var isLoggedIn = profileLink.getAttribute('data-logged-in') === 'true';
+                if (!isLoggedIn) {
+                    return;
+                }
+                event.preventDefault();
+                var dropdownContent = document.querySelector('.dropdown-content');
+                dropdownContent.style.display = 'block';
+            });
+        });
